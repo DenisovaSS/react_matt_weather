1d766177d6cd19c37f2f0be5514bb9179d0660d7
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-01-09 12:33:03 +0000

    skeleton first

--- src/Weather.js
+++ src/Weather.js
@@ -4,12 +4,39 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter your city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Dnipro</h1>
+      <ul>
+        <li>Wensday 07:00</li>
+        <li>Nosty Cloudy</li>
+      </ul>
       <div className="row">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
             alt="cloudy"
-          />
+          />{" "}
+          3C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
         </div>
       </div>
     </div>
