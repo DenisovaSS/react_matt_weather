658fb276094c31bb0d99d6cd1fb0d13ffd8a7256
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-01-25 11:33:26 +0000

    fixed bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,7 @@ export default function WeatherForecast(props) {
     //console.log(response.data);
   }
   if (loaded) {
-    console.log(forecast);
+    //  console.log(forecast);
     return (
       <div className="WeatherForecast">
         <div className="row">
@@ -37,8 +37,8 @@ export default function WeatherForecast(props) {
     );
   } else {
     let apiKey = "e947cb2640f1db92e6a19005bc43b435";
-    let latitude = props.coordinates.lon;
-    let longitude = props.coordinates.lat;
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
     let IpUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(IpUrl).then(handleResponse);
     return null;
